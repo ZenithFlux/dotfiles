c29d712e6449a5cc9cce3c99331808e36bbef9af
Chaitanya Lakhchaura <90706514+ZenithFlux@users.noreply.github.com>
2024-04-17 08:05:12 +0530

    new_session.sh bug fixes and new tmux keymap
    Bugs:
    - Whenever $dir was empty, the script was creating a "-c" session.
    - Unexpected behaviour when $dir contained spaces.

--- tmux/new_session.sh
+++ tmux/new_session.sh
@@ -1,14 +1,15 @@
 #!usr/bin/env bash
 
 dir=`printf "$(pwd)\n$(find ~/personal ~/work ~/other -maxdepth 1 -type d,l)" | fzf`
+[ -z "$dir" ] && dir=$HOME
 name=$(basename "$dir" | tr . _)
 
-cd $dir
+cd "$dir"
 
 if [ -z $TMUX ]; then
-    tmux new -A -s $name -c $dir
+    tmux new -A -s "$name" -c "$dir"
 else
-    tmux new -d -s $name -c $dir
-    tmux switch-client -t $name
+    tmux new -d -s "$name" -c "$dir"
+    tmux switch-client -t "$name"
     exit 0
 fi
