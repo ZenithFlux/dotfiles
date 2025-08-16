ef19f2e754a1cf2a2ce450f77a900b2019f1aeae
Chaitanya Lakhchaura <90706514+ZenithFlux@users.noreply.github.com>
2025-08-16 11:33:11 +0530

    Esc fix in new_session.sh

--- config/tmux/new_session.sh
+++ config/tmux/new_session.sh
@@ -1,12 +1,12 @@
 #!usr/bin/env bash
 
-dir=`printf "$(pwd)\n$(find ~/personal ~/work ~/other -maxdepth 1 -type d,l)" | fzf`
-[ -z "$dir" ] && dir=$HOME
+dir=$(printf "${PWD}\n${HOME}\n$(find ~/personal ~/work ~/other -maxdepth 1 -type d,l)" | fzf)
+if [ -z "$dir" ]; then
+    [ -z "$TMUX" ] && return || exit 0
+fi
 name=$(basename "$dir" | tr . _)
 
-cd "$dir"
-
-if [ -z $TMUX ]; then
+if [ -z "$TMUX" ]; then
     exec tmux new -A -s "$name" -c "$dir"
 else
     tmux new -d -s "$name" -c "$dir"
