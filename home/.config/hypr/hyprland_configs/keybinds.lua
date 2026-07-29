044538ea44fc65e871a0b4646162b6b460b42baf
Chaitanya Lakhchaura <90706514+ZenithFlux@users.noreply.github.com>
2026-07-29 23:57:10 +0530

    logind lid lock

--- home/.config/hypr/hyprland_configs/keybinds.lua
+++ home/.config/hypr/hyprland_configs/keybinds.lua
@@ -71,3 +71,5 @@ hl.bind("XF86AudioNext",  hl.dsp.exec_cmd("playerctl next"),       { locked = tr
 hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
+
+hl.bind("switch:off:Lid Switch", hl.dsp.dpms({ action = "enable" }), { locked = true })
